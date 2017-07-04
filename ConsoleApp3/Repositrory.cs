8c428958b716a12e735feb72fdfafd825ec4a442
benoi <benoit.goethals@gmail.com>
2017-07-04 21:48:35 +0200

    r

--- ConsoleApp3/Repositrory.cs
+++ ConsoleApp3/Repositrory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using MongoDB.Driver;
@@ -9,8 +10,8 @@ namespace ConsoleApp3
     sealed class Repositrory<T> where T : class, new()
     {
         private static readonly Repositrory<T> Instance = new Repositrory<T>();
-        private static readonly MongoClient Client = new MongoClient(Resource1.mongodb.ToString());
-        private static readonly IMongoDatabase DbDatabase=Client.GetDatabase("db" + new T().GetType().Name);
+       
+        private static readonly IMongoDatabase DbDatabase=MongoDbConnection.GetInstance().Client.GetDatabase("db" + new T().GetType().Name);
         private static readonly IMongoCollection<T> PostsCol = DbDatabase.GetCollection<T>("db" + new T().GetType().Name);
        
 
@@ -18,8 +19,8 @@ namespace ConsoleApp3
         public  List<T> All()
         {
             
-          
-            return IAsyncCursorSourceExtensions.ToList<T>(PostsCol.AsQueryable<T>());
+         
+           return IAsyncCursorSourceExtensions.ToList<T>(PostsCol.AsQueryable<T>());
         }
 
 
@@ -35,12 +36,16 @@ namespace ConsoleApp3
             PostsCol.InsertMany(list);
         }
 
+        public List<T> Get()
+        {
+            return null;
+        }
 
 
         public long Count()
         {
-          
-            return PostsCol.AsQueryable().Count<T>();
+        
+           return PostsCol.AsQueryable().Count<T>();
         }
 
         public void RemoveAll()
@@ -51,10 +56,10 @@ namespace ConsoleApp3
 
         }
 
-
-
-
-
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
 
 
         public static Repositrory<T> GetInstance()
---
+++ ConsoleApp3/mongoDbConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+
+namespace ConsoleApp3
+{
+    public sealed class MongoDbConnection
+    {
+        private static readonly MongoDbConnection Instance=new MongoDbConnection();
+
+        private static readonly MongoClient MongoClient = new MongoClient(Resource1.mongodb.ToString());
+
+        public MongoClient Client => MongoClient;
+
+        public static MongoDbConnection GetInstance()
+        {
+            return Instance;
+        }
+    }
+}
